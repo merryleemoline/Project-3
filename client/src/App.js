8a5967f327ed310e3668ee3289d0058a0ce593ea
Merry Moline <merry.l.moline@biola.edu>
2019-11-25 12:58:40 -0800

    changed boilerplate to very basic; started creating more folders for organization

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React from "react";
-import Books from "./pages/Books";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <React.Fragment>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </React.Fragment>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
